35810696bb823055900932d6504baadfc4b1df11
Adrian <adrian@Adrians-MacBook-Pro.local>
2023-09-21 16:36:36 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js
+const registrationForm = document.getElementById("registrationForm");
+
+registrationForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    // You can handle the form submission here, for example, by sending the data to a server.
+    // You can access the form fields like this:
+    const name = document.getElementById("name").value;
+    const course = document.getElementById("course").value;
+    const year = document.getElementById("year").value;
+    const gender = document.querySelector('input[name="gender"]:checked').value;
+    const address = document.getElementById("address").value;
+    const email = document.getElementById("email").value;
+    const contactNumber = document.getElementById("contactNumber").value;
+
+    // Now you can send this data to your server or perform other actions as needed.
+});
