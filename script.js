7fb273a3969ac475ea0f1bcf29ea3f7b286d866c
Adrian <adrian@Adrians-MacBook-Pro.local>
2023-09-21 17:04:33 +0800

    Added Alerts for Submit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// script.js
 const registrationForm = document.getElementById("registrationForm");
 
 registrationForm.addEventListener("submit", function (e) {
     e.preventDefault();
+    
     // You can handle the form submission here, for example, by sending the data to a server.
     // You can access the form fields like this:
     const name = document.getElementById("name").value;
@@ -14,4 +14,8 @@ registrationForm.addEventListener("submit", function (e) {
     const contactNumber = document.getElementById("contactNumber").value;
 
     // Now you can send this data to your server or perform other actions as needed.
+    
+    // Display an alert when the form is submitted.
+    alert("Registration Successful!");
+    alert(`Welcome! ${name}`);
 });
